fa777c5dbf688dda4902b1968a58bf7a590ceb01
heroK12 <koudai2517@gmail.com>
2020-09-15 16:36:10 +0900

    UDP通信用ツール

---
+++ UDP_Client/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace UDP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RcvStart = new System.Windows.Forms.Button();
+            this.IPtxt = new System.Windows.Forms.TextBox();
+            this.LocalPort = new System.Windows.Forms.TextBox();
+            this.RemotePort = new System.Windows.Forms.TextBox();
+            this.RcvClose = new System.Windows.Forms.Button();
+            this.SendBtn = new System.Windows.Forms.Button();
+            this.SendMsg = new System.Windows.Forms.TextBox();
+            this.Logtxt = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RcvStart
+            // 
+            this.RcvStart.Location = new System.Drawing.Point(208, 19);
+            this.RcvStart.Name = "RcvStart";
+            this.RcvStart.Size = new System.Drawing.Size(75, 23);
+            this.RcvStart.TabIndex = 0;
+            this.RcvStart.Text = "受信";
+            this.RcvStart.UseVisualStyleBackColor = true;
+            this.RcvStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // IPtxt
+            // 
+            this.IPtxt.Location = new System.Drawing.Point(63, 15);
+            this.IPtxt.Name = "IPtxt";
+            this.IPtxt.Size = new System.Drawing.Size(132, 19);
+            this.IPtxt.TabIndex = 1;
+            this.IPtxt.Text = "127.0.0.1";
+            // 
+            // LocalPort
+            // 
+            this.LocalPort.Location = new System.Drawing.Point(63, 21);
+            this.LocalPort.Name = "LocalPort";
+            this.LocalPort.Size = new System.Drawing.Size(132, 19);
+            this.LocalPort.TabIndex = 2;
+            // 
+            // RemotePort
+            // 
+            this.RemotePort.Location = new System.Drawing.Point(63, 55);
+            this.RemotePort.Name = "RemotePort";
+            this.RemotePort.Size = new System.Drawing.Size(132, 19);
+            this.RemotePort.TabIndex = 3;
+            // 
+            // RcvClose
+            // 
+            this.RcvClose.Location = new System.Drawing.Point(208, 51);
+            this.RcvClose.Name = "RcvClose";
+            this.RcvClose.Size = new System.Drawing.Size(75, 23);
+            this.RcvClose.TabIndex = 4;
+            this.RcvClose.Text = "閉じる";
+            this.RcvClose.UseVisualStyleBackColor = true;
+            this.RcvClose.Click += new System.EventHandler(this.RcvClose_Click);
+            // 
+            // SendBtn
+            // 
+            this.SendBtn.Location = new System.Drawing.Point(447, 71);
+            this.SendBtn.Name = "SendBtn";
+            this.SendBtn.Size = new System.Drawing.Size(75, 23);
+            this.SendBtn.TabIndex = 5;
+            this.SendBtn.Text = "送信";
+            this.SendBtn.UseVisualStyleBackColor = true;
+            this.SendBtn.Click += new System.EventHandler(this.SendBtn_Click);
+            // 
+            // SendMsg
+            // 
+            this.SendMsg.Location = new System.Drawing.Point(6, 46);
+            this.SendMsg.Name = "SendMsg";
+            this.SendMsg.Size = new System.Drawing.Size(516, 19);
+            this.SendMsg.TabIndex = 6;
+            // 
+            // Logtxt
+            // 
+            this.Logtxt.FormattingEnabled = true;
+            this.Logtxt.ItemHeight = 12;
+            this.Logtxt.Location = new System.Drawing.Point(12, 256);
+            this.Logtxt.Name = "Logtxt";
+            this.Logtxt.Size = new System.Drawing.Size(541, 160);
+            this.Logtxt.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.IPtxt);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 44);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "受信";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IPアドレス";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.LocalPort);
+            this.groupBox2.Controls.Add(this.RcvStart);
+            this.groupBox2.Controls.Add(this.RemotePort);
+            this.groupBox2.Controls.Add(this.RcvClose);
+            this.groupBox2.Location = new System.Drawing.Point(12, 61);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(289, 80);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "送信";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Local";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Remote";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.SendBtn);
+            this.groupBox3.Controls.Add(this.SendMsg);
+            this.groupBox3.Location = new System.Drawing.Point(12, 150);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(541, 100);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "データ送信";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "送信するデータ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Logtxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RcvStart;
+        private System.Windows.Forms.TextBox IPtxt;
+        private System.Windows.Forms.TextBox LocalPort;
+        private System.Windows.Forms.TextBox RemotePort;
+        private System.Windows.Forms.Button RcvClose;
+        private System.Windows.Forms.Button SendBtn;
+        private System.Windows.Forms.TextBox SendMsg;
+        private System.Windows.Forms.ListBox Logtxt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ UDP_Client/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDP_Client
+{
+    public partial class Form1 : Form
+    {
+        public UdpClient udp;
+        IPEndPoint localEP;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //受信ボタン
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListenBroadcastMessage();
+        }
+
+        //受信
+        private void ListenBroadcastMessage()
+        {
+            if (IPtxt.Text == "" || LocalPort.Text == "" || RemotePort.Text == "")
+            {
+                MessageBox.Show("未入力項目があります", "", MessageBoxButtons.OK);
+            }
+            else
+            {
+                try
+                {
+                    //バインドするローカルIPとポート番号
+                    string localIpString = IPtxt.Text;
+                    IPAddress localAddress = IPAddress.Parse(localIpString);
+                    int localPort = int.Parse(LocalPort.Text);
+
+                    //受信ボタン
+                    RcvStart.Enabled = false;
+                    RcvClose.Enabled = true;
+
+                    //UdpClientを作成し、ローカルエンドポイントにバインドする IP + Port
+                    IPEndPoint localEP = new IPEndPoint(localAddress, localPort);
+
+                    //ソケットおーぷん
+                    udp = new UdpClient(localEP);
+
+                    //データ受信する
+                    var rcvresult = udp.BeginReceive(ReceiveCallback, null);
+
+                    Logtxt.Items.Insert(0, "受信開始");
+
+                }catch(SocketException e)
+                {
+                    MessageBox.Show("既にそのポートが使われている可能性があります。");
+                }
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            UdpClient udpClient = udp;
+            IPEndPoint endPoint = localEP;
+            byte[] receiveByte;
+            try
+            {
+                receiveByte = udpClient.EndReceive(ar, ref endPoint);
+            }
+            catch(SocketException)
+            {
+                CloseSocket();
+                MessageBox.Show("通信エラー","",MessageBoxButtons.OK);
+
+                return;
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
+
+            //文字列に変換
+            string receiveString = Encoding.UTF8.GetString(receiveByte);
+
+            //時刻取得
+            DateTime dt = DateTime.Now;
+
+            //時間フォーマット
+            string rcvtime = dt.ToString("yyyy/MM/dd HH:mm:ss");
+
+            //Task実行の忠告非表示
+            ListBox.CheckForIllegalCrossThreadCalls = false;
+
+            //受信したデータと送信者の情報を表示する
+            Logtxt.Items.Insert(0, rcvtime + " " + "【RECV】" + receiveString + "\r\n");
+
+            //ループ処理
+            udpClient.BeginReceive(ReceiveCallback, udp);
+
+            return;
+        }
+
+        //閉じる
+        private void RcvClose_Click(object sender, EventArgs e)
+        {
+            CloseSocket();
+        }
+
+        //ソケット閉じる処理
+        private void CloseSocket()
+        {
+            if (udp != null)
+            {
+                udp.Close();
+                Invoke((MethodInvoker)(() =>
+                Logtxt.Items.Insert(0, "受信終了")));
+                Invoke((MethodInvoker)(() =>
+                RcvStart.Enabled = true));
+                Invoke((MethodInvoker)(() =>
+                RcvClose.Enabled = false));
+            }
+            else
+            {
+                Invoke((MethodInvoker)(() =>
+                RcvStart.Enabled = true));
+                Invoke((MethodInvoker)(() =>
+                RcvClose.Enabled = false));
+            }
+        }
+        //送信
+        private void SendBtn_Click(object sender, EventArgs e)
+        {
+            sendmessage();
+        }
+
+        private void sendmessage()
+        {
+            //データを送信するリモートホストとリモートポート
+            string remoteHost = IPtxt.Text;
+            int remotePort = int.Parse(RemotePort.Text);
+
+            //バインドするローカルポート番号
+            int localPort = int.Parse(LocalPort.Text);
+
+            //ローカルポート番号localportにバインドする
+            UdpClient udp = new UdpClient(localPort);
+
+            //送信するデータを作成する
+            string strmsg = SendMsg.Text;
+
+            //変換
+            byte[] sendBytes = Encoding.UTF8.GetBytes(strmsg.ToString());
+
+            //時間取得
+            DateTime dt = DateTime.Now;
+
+            //時刻フォーマット
+            string sendtime = dt.ToString("yyyy/MM/dd HH:mm:ss");
+
+            try
+            {
+                //リモートホストを指定してデータを送信する
+                udp.Send(sendBytes, sendBytes.Length, remoteHost, remotePort);
+
+                Logtxt.Items.Insert(0, sendtime + "  " + "【SEND】:" + strmsg + "\r\n");
+
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            udp.Close();
+        }
+        
+
+    }
+}
